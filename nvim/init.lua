40e954e464254bb50db905e644b993da8bd9bf9f
Spencer Hann <spencer.hann@gmail.com>
2026-04-14 12:35:29 -0700

    init.lua cleanup

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,12 +2,14 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
 
+-- vim.opt.runtimepath:remove("~/.config/nvim")
+-- vim.opt.runtimepath:prepend("~/dotfiles/nvim")
+-- vim.opt.runtimepath = vim.opt.runtimepath:get()
+
 require('config.options')
 require('config.global_keymaps')
 
 require('config.color')
 require('config.pack')
--- require('config.keymaps')
 require('config.diagnostics')
--- require('config.autocmds')
 
