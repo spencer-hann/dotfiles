e0a0690bae95b247155809351f694cfeaae76ad6
Spencer Hann <spencer.hann@gmail.com>
2026-04-30 10:21:51 -0700

    add clangd language server

--- nvim/lua/config/lsp/lsp.lua
+++ nvim/lua/config/lsp/lsp.lua
@@ -179,10 +179,11 @@ return function()
         require("mason-lspconfig").setup()
         require("mason-tool-installer").setup({
                 ensure_installed = {
-                        "lua_ls",
-                        "stylua",
                         "basedpyright",
+                        "clangd",
+                        "lua_ls",
                         "rust-analyzer",
+                        "stylua",
                 },
         })
 
